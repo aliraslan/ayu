feb3a0f9a25f714418a4bd8160e9068185d21358
Raslan <24810123+aliraslan@users.noreply.github.com>
2020-02-12 21:18:46 +0200

    Added files

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo mv Ayu-Plasma /usr/share/plasma/desktoptheme/Ayu
+sudo mv Ayu-GTK3 /usr/share/themes/Ayu-GTK3
+sudo mv Ayu-GTK2 /usr/share/themes/Ayu-GTK2
+mv Ayu.colors /.kde4/share/apps/color-schemes/Ayu.colors
+cd ..
+rm -rf Ayu
